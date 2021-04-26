d95b190598d142c0eb37bf2af9729eab840480b3
ABojo <alex.bojorquez2598@gmail.com>
2021-04-26 17:55:49 -0400

    Wrote the caesar cipher function and it passed all tests

--- functions/caesarCipher.js
+++ functions/caesarCipher.js
@@ -1,3 +1,26 @@
-const caesarCipher = () => {};
+const shiftChar = (charNum, shiftNum, lastCharNum) => {
+  return charNum + shiftNum <= lastCharNum
+    ? charNum + shiftNum
+    : charNum + shiftNum - 26;
+};
+
+const shiftCodes = (charCodes, shiftNum) => {
+  const upperZCode = 90;
+  const lowerZCode = 122;
+
+  return charCodes.map((charNum) => {
+    if (charNum === 32) return 32;
+    if (charNum <= upperZCode) return shiftChar(charNum, shiftNum, upperZCode);
+    if (charNum <= lowerZCode) return shiftChar(charNum, shiftNum, lowerZCode);
+  });
+};
+
+const caesarCipher = (string, shiftNum) => {
+  const charCodes = string.split('').map((letter) => letter.charCodeAt(0));
+  const newCodes = shiftCodes(charCodes, shiftNum);
+  const cipherString = String.fromCharCode(...newCodes);
+
+  return cipherString;
+};
 
 module.exports = caesarCipher;
