c274e3203010048b08c4e6415eacdb4704a81b08
ABojo <alex.bojorquez2598@gmail.com>
2021-04-26 18:32:12 -0400

    Wrote the function to return an object that contains the analysis of the array to pass all tests

--- functions/analyze.js
+++ functions/analyze.js
@@ -1,3 +1,10 @@
-const analyze = (array) => {};
+const analyze = (array) => {
+  return {
+    average: array.reduce((acc, cv) => acc + cv, 0) / array.length,
+    length: array.length,
+    min: Math.min(...array),
+    max: Math.max(...array),
+  };
+};
 
 module.exports = analyze;
