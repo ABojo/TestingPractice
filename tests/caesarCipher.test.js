eee84d424bc48dc4812bd0b437877e1977fbb480
ABojo <alex.bojorquez2598@gmail.com>
2021-04-26 17:23:52 -0400

    rewrote tests to include a shift arguement

--- tests/caesarCipher.test.js
+++ tests/caesarCipher.test.js
@@ -1,13 +1,13 @@
 const caesarCipher = require('../functions/caesarCipher');
 
 test('Shift the word "Testing"', () => {
-  expect(caesarCipher('Testing')).toBe('Alzapun');
+  expect(caesarCipher('Testing', 7)).toBe('Alzapun');
 });
 
 test('Shift the phrase "The Odin Project"', () => {
-  expect(caesarCipher('The Odin Project')).toBe('Aol Vkpu Wyvqlja');
+  expect(caesarCipher('The Odin Project', 10)).toBe('Dro Ynsx Zbytomd');
 });
 
 test('Shift the phrase "tEsT tHe CaSe"', () => {
-  expect(caesarCipher('tEsT tHe CaSe')).toBe('aLzA aOl JhZl');
+  expect(caesarCipher('tEsT tHe CaSe', 4)).toBe('xIwX xLi GeWi');
 });
